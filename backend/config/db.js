9013dc8d6934635f16a6dce83c3cf078555c43b2
abirami <81287215+abi-sender@users.noreply.github.com>
2026-07-14 06:38:03 -0500

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,13 @@
 import mongoose from "mongoose";
 
-// MongoDB Connection
-const connectDB=async()=>
-{
-    try
-    {
-    await mongoose.connect("mongodb://127.0.0.1:27017/addcartdb");
-    console.log("MongoDB Connected")
-    }
-  catch(err)
-  {
-console.log(err);
-  } 
-}
+const connectDB = async () => {
+  try {
+
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export default connectDB;
