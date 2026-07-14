99b4e9ca456c5e6bae27684d8aa7638d949bbda6
abirami <81287215+abi-sender@users.noreply.github.com>
2026-07-14 06:14:49 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
@@ -10,9 +13,10 @@ app.use(express.json());
 
 connectDB();
 
-// Use cart routes
 app.use("/", cartRoutes);
 
-app.listen(5000, () => {
-  console.log("Server Running on Port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${PORT}`);
 });
